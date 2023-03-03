46f47b1d4b7986557d78fefb70522167cbe2a3fd
Nguyễn Đức Đương - 2121050266 <125018793+nddhumg@users.noreply.github.com>
2023-03-03 17:08:00 +0700

    Create Bài9.java

---
+++ Tuần5/Bài9.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Bai9 {
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap so luong phan tu:");
+		String a = sc.nextLine();
+		int t=0,h=0,s=0;
+		char kytu;
+		for(int i=0;i<a.length();i++) {
+			kytu = a.charAt(i);
+			if(kytu>=48 && kytu<=57) {
+				s++;
+			}else if(kytu>=65 && kytu<=90) {
+				h++;
+			}else if(kytu>=97 && kytu<=122) {
+				t++;
+			}
+		}
+		System.out.println("So chu Hoa trong string:"+ h );
+		System.out.println("So chu Thuong trong string:"+ t );
+		System.out.println("So chu So trong string:"+ s );
+	}	 
+}
